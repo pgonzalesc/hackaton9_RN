258a91790408145c3dadb55e8ed1d8853f2ac595
pgonzalesc <pedro281195@gmail.com>
2020-12-05 12:06:44 -0500

    Avance

---
+++ components/screens/Finance/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Finance = () => {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Mis finanzas</Text>
+        </View>
+    )
+}
+
+export default Finance;
